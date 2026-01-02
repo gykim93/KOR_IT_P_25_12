9c6ee855af6249daa8406e454489c1b1f2bd2676
gykim93 <gykim1993@gmail.com>
2026-01-02 12:29:31 +0900

    while문

--- pyhon_exam.py
+++ pyhon_exam.py
@@ -1,29 +1,54 @@
-# 문제 : 구구단 8단을 출력해주세요.
-# 조건 : for, while문을 사용할 수 없습니다.
-
-"""
-출력 양식
-== 8단 ==
-8 * 1 = 8
-8 * 2 = 16
-8 * 3 = 24
-8 * 4 = 32
-8 * 5 = 40
-8 * 6 = 48
-8 * 7 = 56
-8 * 8 = 64
-8 * 9 = 72
-"""
+# 구구단 8단 제작과정
 print("== v1 ==")
 print("8 * 1 = 8")
 print("8 * 2 = 16")
 print("8 * 3 = 24")
 
+
 print("== v2 ==")
-print("8 * 1 = 8\n8 * 2 = 16\n8 * 3 = 24")
+print(f"{8} * {1} = {8}")
+print(f"{8} * {2} = {16}")
+print(f"{8} * {3} = {24}")
+
 print("== v3 ==")
-s = ""
-s = s + "8 * 1 = 8"
-s = s + "\n8 * 2 = 16"
-s = s + "\n8 * 3 = 24"
-print(s)
+dan = 8
+print(f"{dan} * {1} = {dan * 1}")
+print(f"{dan} * {2} = {dan * 2}")
+print(f"{dan} * {3} = {dan * 3}")
+
+print("== v4 ==")
+dan = 8
+i = 1
+print(f"{dan} * {i} = {dan * i}")
+i = i + 1
+print(f"{dan} * {i} = {dan * i}")
+i += 1
+print(f"{dan} * {i} = {dan * i}")
+
+print("== v5 ==")
+dan = 8
+i = 1
+if i <= 3:
+    print(f"{dan} * {i} = {dan * i}")
+    i += 1
+if i <= 3:
+    print(f"{dan} * {i} = {dan * i}")
+    i += 1
+if i <= 3:
+    print(f"{dan} * {i} = {dan * i}")
+    i += 1
+if i <= 3:
+    print(f"{dan} * {i} = {dan * i}")
+    i += 1
+    
+# 빈복문 시작
+# while 문
+# 1 ~ 3 까지 출력
+# 초기값
+# 종료조건
+# 보폭
+i = 1 #초기값
+while i <= 3: # 종료조건
+  print(i)
+  i += 1 #보폭 i = i + 1
+          
