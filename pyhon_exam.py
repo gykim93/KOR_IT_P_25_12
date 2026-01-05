5eb5234ff2dd141b1cce42511dcb467aae04c5d8
gykim93 <gykim1993@gmail.com>
2026-01-05 09:40:21 +0900

    1,2번 문제 정답

--- pyhon_exam.py
+++ pyhon_exam.py
@@ -1,54 +1,11 @@
-# 구구단 8단 제작과정
-print("== v1 ==")
-print("8 * 1 = 8")
-print("8 * 2 = 16")
-print("8 * 3 = 24")
-
-
-print("== v2 ==")
-print(f"{8} * {1} = {8}")
-print(f"{8} * {2} = {16}")
-print(f"{8} * {3} = {24}")
-
-print("== v3 ==")
-dan = 8
-print(f"{dan} * {1} = {dan * 1}")
-print(f"{dan} * {2} = {dan * 2}")
-print(f"{dan} * {3} = {dan * 3}")
-
-print("== v4 ==")
-dan = 8
+# 문제 : 1부터 300까지 출력해주세요.
 i = 1
-print(f"{dan} * {i} = {dan * i}")
-i = i + 1
-print(f"{dan} * {i} = {dan * i}")
-i += 1
-print(f"{dan} * {i} = {dan * i}")
+while i <= 300:
+    print(i)
+    i += 1
 
-print("== v5 ==")
-dan = 8
-i = 1
-if i <= 3:
-    print(f"{dan} * {i} = {dan * i}")
+# 문제 - 500부터 2330까지 출력해주세요.(개념 : while)    
+i = 500
+while i <= 2330:
+    print(i)
     i += 1
-if i <= 3:
-    print(f"{dan} * {i} = {dan * i}")
-    i += 1
-if i <= 3:
-    print(f"{dan} * {i} = {dan * i}")
-    i += 1
-if i <= 3:
-    print(f"{dan} * {i} = {dan * i}")
-    i += 1
-    
-# 빈복문 시작
-# while 문
-# 1 ~ 3 까지 출력
-# 초기값
-# 종료조건
-# 보폭
-i = 1 #초기값
-while i <= 3: # 종료조건
-  print(i)
-  i += 1 #보폭 i = i + 1
-          
