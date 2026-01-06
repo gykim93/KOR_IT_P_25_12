e9a84b08ba798178d60fb64de30c7128dd4767b7
gykim93 <gykim1993@gmail.com>
2026-01-06 11:49:53 +0900

    문제 8 ~ 10

--- pyhon_exam.py
+++ pyhon_exam.py
@@ -1,7 +1,36 @@
-# 문제 : 입력받은 정수가 짝수인지 아닌지 판별해주는 함수 구현
-def is_even(num):
-    return num % 2 == 0
-print(f"10은(는) 짝수인가요? : {is_even(10)}\n")
-print(f"11은(는) 짝수인가요? : {is_even(11)}\n")
-print(f"25은(는) 짝수인가요? : {is_even(25)}\n")
-print(f"15은(는) 짝수인가요? : {is_even(15)}\n")
+# 문제 : 입력받은 정수가 3의 배수인지 알려주는 함수 구현
+def is_3_multiple(num):
+    return num % 3 == 0
+
+print(f"10은(는) 3의 배수인가요? : {is_3_multiple(10)}\n")
+print(f"12은(는) 3의 배수인가요? : {is_3_multiple(12)}\n") 
+
+
+
+
+
+#문제 : 입력받은 정수가 100보다 큰지 알려주는 함수 구현
+def is_bigger_than_100(num):
+    return num > 100
+
+print(f"128은(는) 100보다 큽니다. : {is_bigger_than_100(128)}\n")
+print(f"28은(는) 100보다 큽니다. : {is_bigger_than_100(28)}\n")
+print(f"100은(는) 100보다 큽니다. : {is_bigger_than_100(100)}\n") 
+
+
+ # 문제 : 입력받은 정수의 모든 약수의 합을 리턴하는 함수를 구현해주세요.
+
+def get_divisors_sum(num):
+    s = 0
+    i = 1
+    while i <= num:
+        if num % i == 0:
+            s += i
+        i += 1
+    return s
+
+s = get_divisors_sum(1000)
+
+print(f"정수 1000의 약수의 합 : {s}")
+# 출력 => 정수 1000의 약수의 합 : 2340 
+ 
