030a63ea6c69b70e04eb23b6b0a804fa27102378
gykim93 <gykim1993@gmail.com>
2026-01-06 11:24:09 +0900

    문제6

--- pyhon_exam.py
+++ pyhon_exam.py
@@ -1,44 +1,13 @@
-# 문제 : 5칙연산을 수행하는 함수를 만들어주세요.
+# 문제 : 온도단위 섭씨를, 화씨로 바꿔주는, 함수 c_to_f 를 구현해주세요.
+# 조건 : 공식 = 섭씨온도 * (9 / 5) + 32 => 화씨온도
 
-# plus 함수 구현
-def plus(a, b):
-    return a + b
+def c_to_f(c):
+  f = c * (9 / 5) + 32
+  return f
 
-print(f"3 더하기 5는 {plus(3, 5)} 입니다.")
-# 출력 => 3 더하기 5는 8 입니다.
-
-# plus_3_nums 함수 구현
-def plus_3_nums(a, b, c):
-    return a + b + c
-print(f"3 더하기 5 더하기 7은 {plus_3_nums(3, 5, 7)} 입니다.")
-# 출력 => 3 더하기 5 더하기 7은 15 입니다.
-
-# minus 함수 구현
-def minus(a, b):
-    return a - b
-print(f"10 빼기 5 는 {minus(10, 5)} 입니다.")
-# 출력 => 10 빼기 5 는 5 입니다.
-
-# multiply 함수 구현
-def multiply(a , b):
-    return a * b
-print(f"10 곱하기 5 는 {multiply(10, 5)} 입니다.")
-# 출력 => 10 곱하기 5 는 50 입니다.
-
-# mod 함수 구현
-def mod(a, b):
-    return a % b
-print(f"4를 3으로 나눈 나머지는 {mod(4, 3)} 입니다.")
-# 출력 => 4를 3으로 나눈 나머지는 1 입니다.
-
-# div 함수 구현
-def div(a, b):
-    return a // b
-print(f"4를 3으로 나눈 몫은 {div(4, 3)} 입니다.")
-# 출력 => 4를 3으로 나눈 몫은 1 입니다.
-
-# div2 함수 구현
-def div2(a, b):
-    return a / b
-print(f"4를 3으로 나눈 결과는 {div2(4, 3)} 입니다.")
-# 출력 => 4를 3으로 나눈 결과는 1.3333333333333333 입니다.
+print(c_to_f(10))
+# 출력 => 50.0
+print(c_to_f(20))
+# 출력 => 68.0
+print(c_to_f(30))
+# 출력 => 86.0
