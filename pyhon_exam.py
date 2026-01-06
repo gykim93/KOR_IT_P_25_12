ffa1588780e12c5c7d15074207f3ec93a140094e
gykim93 <gykim1993@gmail.com>
2026-01-06 10:15:22 +0900

    문제3

--- pyhon_exam.py
+++ pyhon_exam.py
@@ -1,10 +1,24 @@
-# 문제 : 함수를 사용해서 코드량을 확 줄여주세요.
-def print_dan(dan, limit): # 괄호 안쪽에 있는 dan, limit => 매개변수
-    print(f"== {dan}단 ==")
-    i = 1
-    while i <= limit:
-        print(f"{dan} * {i} = {dan * i}")
-        i += 1
-        
-print_dan(1, 5) # 1단 * 5 => 1, 5 각각 1은 dan, 5 => limit
-print_dan(2, 13) # 2단 * 13       
+# 문제 : 매개변수의 개수를 맞춰주세요.
+# 조건 : 함수호출 시 사용한 인자에 따라 함수를 적절하게 잘 만들어 주세요.
+# 조건 : 오류가 나지 않으면 성공입니다.
+def a():
+    print("함수 a실행")
+
+def b(b1, b2, b3):
+    print("함수 b실행")
+    print(b1)
+    print(b2)
+    print(b3)
+    
+def c(c1, c2, c3, c4):
+    print("함수 c실행")
+    print(c1)
+    print(c2)
+    print(c3)        
+    print(c4)        
+
+
+
+a()
+b(1, 2, 3)
+c("안녕", 1 == 1, 550, 600)
