ebaa5df140d2093c5c54e8deac5e950d1ff3f6bf
gykim93 <gykim1993@gmail.com>
2026-01-06 10:29:51 +0900

    문제4

--- pyhon_exam.py
+++ pyhon_exam.py
@@ -1,24 +1,11 @@
-# 문제 : 매개변수의 개수를 맞춰주세요.
-# 조건 : 함수호출 시 사용한 인자에 따라 함수를 적절하게 잘 만들어 주세요.
-# 조건 : 오류가 나지 않으면 성공입니다.
-def a():
-    print("함수 a실행")
+# 문제 : 입력받은 정수의 모든 약수를 출력하는 함수를 구현해주세요.
+# 약수 => 나눴을 때 나머지가 0
 
-def b(b1, b2, b3):
-    print("함수 b실행")
-    print(b1)
-    print(b2)
-    print(b3)
-    
-def c(c1, c2, c3, c4):
-    print("함수 c실행")
-    print(c1)
-    print(c2)
-    print(c3)        
-    print(c4)        
+def print_divisors(num):
+  i = 1
+  while i <= num:
+      if num % i == 0:
+          print(i)
+      i += 1
 
-
-
-a()
-b(1, 2, 3)
-c("안녕", 1 == 1, 550, 600)
+print_divisors(1000)
