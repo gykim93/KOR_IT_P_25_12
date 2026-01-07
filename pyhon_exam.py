3f7249b719862aec387551cbb4e75996c78e875d
gykim93 <gykim1993@gmail.com>
2026-01-07 10:06:54 +0900

    문제2

--- pyhon_exam.py
+++ pyhon_exam.py
@@ -1,6 +1,4 @@
-# 문제 - 입력받은 정수가 소수인지 아닌지 알려주는 함수를 구현해주세요.(개념 : 함수, 반복문, 리턴)
-# 소수 => 1과 자기자신 제외하고 나눠지지 않는 수
-
+# 소수판별기
 def is_prime_number(num):
     if num == 1:
         return False
@@ -11,10 +9,13 @@ def is_prime_number(num):
         i += 1
     return True # 위 조건과 반복문을 모두 만족하지 않았다면 소수다.
 
-print(f"1은 소수입니다 : {is_prime_number(1)}")        
-print(f"3은 소수입니다 : {is_prime_number(3)}")
-print(f"4는 소수입니다 : {is_prime_number(4)}")
-print(f"5는 소수입니다 : {is_prime_number(5)}")
-print(f"6는 소수입니다 : {is_prime_number(6)}")
-print(f"7는 소수입니다 : {is_prime_number(7)}")
-print(f"1000은 소수입니다 : {is_prime_number(1000)}")
+# 문제 : 1부터 1000사이에 존재하는 소수들의 개수를 출력해주세요.
+
+count = 0
+i = 1
+while i <= 1000:
+    if is_prime_number(i):
+        count += 1
+    i += 1
+    
+print(f"1부터 1000사이에 존재하는 소수들의 개수 : {count}")
