f26eafa74924c73eabe0fb633b33f9f95abb0270
gykim93 <gykim1993@gmail.com>
2026-01-07 11:25:50 +0900

    리스트 객체

--- pyhon_exam.py
+++ pyhon_exam.py
@@ -1,28 +1,32 @@
-# 문제 : 두개의 숫자를 연결해서 새로운 문장을 만들어주세요.
-a = 10 # => "10"
-b = 20 # => "20"
+# 나이 10살 20살 30살
+# age = 10
+# age = 20
+# age = 30
 
-print(str(a) + str(b))
-# 출력 => 1020
+ages = [] # 빈 리스트 객체를 하나 만들었다.
+# 객체를 조종할 수 있는 리모콘 ages라는 변수에 들어간다.
 
-# 문제 : 두개의 숫자문장를 더해서 새 정수를 만들어주세요.
-a = '3' # 3
-b = '6' # 6
+ages2 = ages # ages 변수 안에 들어있는 리모콘이 복사되서 ages2 변수에 들어간다.
 
-print(int(a) + int(b))
-# 출력 9
+ages.append(10) 
+ages.append(20)
+ages.append(30)
 
-# 문제 : 두개의 숫자문장를 더해서 새 실수를 만들어주세요.
-a = '3.1' # 3.1
-b = '6.1' # 6.1
+ages3 = []
 
-print(float(a) + float(b))
-# 출력 9.2
+print(ages)
 
-# 문제 : 2개 실수문장을 정수화 하여 더해주세요.
-# 실수화 => 정수화
-a = '3.1' # 3.1 => 3
-b = '6.1' # 6.1 => 6
+# 위 코드 객체는 2개
+# append() 함수는 파이썬에서 리스트의 끝에 새로운 요소를 추가하는 메서드(함수)
 
-print(int(float(a)) + int(float(b)))
-# 출력 9
+# 리스트생성
+fruits = ['apple', 'banana', 'cherry']
+print(fruits)
+fruits.append('orange')
+print(fruits)
+
+# 리스트 객체 안에 요소 접근 => 인덱스
+print(fruits[0]) 
+print(fruits[1])
+print(fruits[2])
+print(fruits[3])
