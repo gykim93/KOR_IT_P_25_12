0a63775c993d05ad8a99e536e767ad89b7413203
gykim93 <gykim1993@gmail.com>
2026-01-07 10:31:26 +0900

    문제5

--- pyhon_exam.py
+++ pyhon_exam.py
@@ -9,29 +9,29 @@ def is_prime_number(num):
         i += 1
     return True # 위 조건과 반복문을 모두 만족하지 않았다면 소수다.
 
-# 문제 : 1부터 n 사이의 수 중에서 소수의 개수 반환하는 함수 `get_1_to_n_prime_numbers_count` 를 구현해주세요.
+# 문제 : 1부터 n 사이의 수 중에서 소수의 합을 반환하는 함수 `get_1_to_n_prime_numbers_sum` 를 구현해주세요.
 
 
-# 1부터 n 사이의 수 중에서 소수의 개수 반환하는 함수
-def get_1_to_n_prime_numbers_count(n):
-  #여기서 구현해주세요.
-    count = 0
+# 1부터 n 사이의 수 중에서 소수의 합 반환하는 함수
+def get_1_to_n_prime_numbers_sum(n):
+    # 구현
+    sum = 0
     i = 1
     while i <= n:
         if is_prime_number(i):
-            count += 1        
+            sum += i
         i += 1
-    return count
+    return sum
 
-count = 0
+sum = 0
 number = 0
 
 number = 1000
-count = get_1_to_n_prime_numbers_count(number)
-print(f"1부터 {number}사이에 존재하는 소수의 개수 : {count}개\n")
-  # 출력 => 1부터 1000사이에 존재하는 소수의 개수 : 168개
+sum = get_1_to_n_prime_numbers_sum(number)
+print(f"1부터 {number}사이에 존재하는 소수의 합 : {sum}개\n")
+  # 출력 => 1부터 1000사이에 존재하는 소수의 합 : 76127
 
 number = 2000
-count = get_1_to_n_prime_numbers_count(number)
-print(f"1부터 {number}사이에 존재하는 소수의 개수 : {count}개\n")
-  # 출력 => 1부터 2000사이에 존재하는 소수의 개수 : 303개
+sum = get_1_to_n_prime_numbers_sum(number)
+print(f"1부터 {number}사이에 존재하는 소수의 합 : {sum}개\n")
+ # 출력 => 1부터 2000사이에 존재하는 소수의 합 : 277050
