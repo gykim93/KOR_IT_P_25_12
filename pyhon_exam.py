8957f870e1797961b80fb847974652908f3c9aca
gykim93 <gykim1993@gmail.com>
2026-01-07 11:40:27 +0900

    문제 6 ~ 8

--- pyhon_exam.py
+++ pyhon_exam.py
@@ -1,32 +1,17 @@
-# 나이 10살 20살 30살
-# age = 10
-# age = 20
-# age = 30
+# 문제6 : 리스트에 순서대로 2, 1, 5, 6, 7를 한번에 담아주세요.
+a = [2, 1, 5, 6, 7]
 
-ages = [] # 빈 리스트 객체를 하나 만들었다.
-# 객체를 조종할 수 있는 리모콘 ages라는 변수에 들어간다.
+# 문제7 : 비어있는 리스트를 만들고, 순서대로 2, 1, 5, 6, 7를 하나씩 담아주세요.
+b = []
+b.append(2) # 0
+b.append(1) # 1
+b.append(5) # 2
+b.append(6) # 3
+b.append(7) # 4
 
-ages2 = ages # ages 변수 안에 들어있는 리모콘이 복사되서 ages2 변수에 들어간다.
-
-ages.append(10) 
-ages.append(20)
-ages.append(30)
-
-ages3 = []
-
-print(ages)
-
-# 위 코드 객체는 2개
-# append() 함수는 파이썬에서 리스트의 끝에 새로운 요소를 추가하는 메서드(함수)
-
-# 리스트생성
-fruits = ['apple', 'banana', 'cherry']
-print(fruits)
-fruits.append('orange')
-print(fruits)
-
-# 리스트 객체 안에 요소 접근 => 인덱스
-print(fruits[0]) 
-print(fruits[1])
-print(fruits[2])
-print(fruits[3])
+# 문제8 : 리스트에 순서대로 '월', '화', '수', '목', '금'을 한번에 담아주세요. 그리고 '토', '일' 을 순서대로 추가해주세요.
+day = ['월', '화', '수', '목', '금']
+print(day)
+day.append('토')
+day.append('일')
+print(day)
