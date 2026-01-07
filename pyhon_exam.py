8d80d2e805bdfaabcaf0b191a992191303d6ad37
gykim93 <gykim1993@gmail.com>
2026-01-07 10:17:38 +0900

    문제3

--- pyhon_exam.py
+++ pyhon_exam.py
@@ -9,13 +9,13 @@ def is_prime_number(num):
         i += 1
     return True # 위 조건과 반복문을 모두 만족하지 않았다면 소수다.
 
-# 문제 : 1부터 1000사이에 존재하는 소수들의 개수를 출력해주세요.
+#문제 : 입력받은 숫자가 10이라고 할때 1부터 10 사이에 존재하는 모든 소수를 출력하는 함수 구현
 
-count = 0
-i = 1
-while i <= 1000:
-    if is_prime_number(i):
-        count += 1
-    i += 1
-    
-print(f"1부터 1000사이에 존재하는 소수들의 개수 : {count}")
+def print_1_to_n_prime_numbers(num):
+    i = 1
+    while i <= num:
+        if is_prime_number(i):
+            print(i)
+        i += 1
+
+print_1_to_n_prime_numbers(10)
