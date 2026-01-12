1d98a3dc1f4c09a226ddf489eeb848df285ac7c8
gykim93 <gykim1993@gmail.com>
2026-01-12 10:42:21 +0900

    문자열 인덱스 슬라이싱

--- pyhon_exam.py
+++ pyhon_exam.py
@@ -1,25 +1,76 @@
-# 문제 : 리스트에 2, 1, 5, 6, 7를 담고, for문으로 요소 전부 출력, len 사용
-# a = [2, 1, 5, 6, 7]
-# for i in range(len(a)):
-#     print(a[i])
-# 문제 : 리스트에 2, 1, 5, 6, 7를 담고, for문으로 요소들을 역순으로 출력, len 사용
-# a = [2, 1, 5, 6, 7]
-# for i in range(len(a) -1, -1, -1):
-#     print(a[i])
+# print("== 예제 1 ==")
+# s = "안녕하세요."
 
-# 문제 : 리스트에 각 달의 끝 날짜들을 담고, '1월은 31일까지'와 같은 양식으로 출력
-# end_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-# current_month = 1
-# for end_day in end_days:
-#     print(f"{current_month}월은 {end_day}일까지")
-#     current_month += 1
-# 문제 : 리스트에 각 달의 끝 날짜들을 담고, '1월은 31일까지'와 같은 양식으로 출력, len 사용
-# end_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-# for i in range(len(end_days)):
-#     print(f"{i + 1}월은 {end_days[i]}일까지")
-# 문제 : 리스트에 각 달의 끝 날짜들을 담고, '1월은 31일까지'와 같은 양식으로 출력, enumerate 사용
-end_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-# enumerate() 함수 => 리스트
-for i, end_day in enumerate(end_days):
-    month = i + 1 # month 변수에 인덱스 값에 1을 더해서 실제 월
-    print(f"{month}월은 {end_day}일까지")
+# print("s의 길이 출력")
+# print(len(s))
+
+# print("s를 이루는 문자 중에서 앞에서 문자 2개 출력")
+# print(s[0:2])
+
+# print("s를 이루는 문자 중에서 앞에서 문자 하나 건너 뛰고 1개 출력")
+# print(s[1:2])
+
+# print("s를 이루는 문자 중에서 앞에서 문자 4개 출력, 첫 인덱스 생략")
+# print(s[:4])
+
+# print("s를 이루는 문자 중에서 앞에서 문자 3개 건너뛰고, 나머지 전부 출력, len")
+# print(s[3:len(s)])
+
+# print("s를 이루는 문자 중에서 앞에서 문자 3개 건너뛰고, 나머지 전부 출력, 뒤 인덱스 생략, 방법 2")
+# print(s[3:])
+
+# print("== 예제 2 ==")
+# s = "안녕하세요"
+
+# print("s를 이루는 문자를 하나씩 출력, while")
+# i = 0
+# while i < len(s):
+#     print(s[i])
+#     i += 1
+
+# print("s를 이루는 문자를 역순으로 하나씩 출력, while")
+# i = len(s) -1
+# while i >= 0:
+#     print(s[i])
+#     i -= 1
+
+# print("s를 이루는 문자를 하나씩 출력, for")
+# for c in s:
+#     print(c)
+
+# print("== 예제 3 ==")
+# s = "a안b녕c하d세e요f"
+
+# print("s를 이루는 문자 중에서 한글만 출력")
+# print(s[1] + s[3] + s[5] + s[7] + s[9])
+
+# print("s를 이루는 문자 중에서 한글만 출력, for, if or, continue")
+# for c in s:
+#     if c == "a" or c == "b" or c == "c" or c == "d" or c == "e" or c == "f":
+#         # 현재 문자가 a ~ f중에 하나인지 확인
+#         continue  # 위 조건이 참이면 현재 루프를 건너 뛴다.
+#     print(c)
+
+# print("s를 이루는 문자 중에서 한글만 출력, for, if and, 방법 2")
+# for c in s:
+#     if c != "a" and c != "b" and c != "c" and c != "d" and c != "e" and c != "f":
+#         print(c)
+
+# print("s를 이루는 문자 중에서 한글만 출력, for, not in, 방법 3")
+# for c in s:
+#     if c not in ["a", "b", "c", "d", "e", "f"]: # 현재 문자가 a ~ f 리스트에 있는지 없는지 확인
+#         print(c)
+
+# continue
+# print("== continue ==")
+# for i in range(1, 6):
+#     if i == 3:
+#         continue
+#     print(i)
+
+# break
+# print("== break ==")
+# for i in range(1, 6):
+#     if i == 3:
+#         break
+#     print(i)
