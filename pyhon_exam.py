2c8c8e4dcf5e584f0242ce6cc97d6337d6b7a31a
gykim93 <gykim1993@gmail.com>
2026-01-13 09:27:24 +0900

    데이터 입출력

--- pyhon_exam.py
+++ pyhon_exam.py
@@ -1,76 +1,65 @@
-# print("== 예제 1 ==")
-# s = "안녕하세요."
+# 전역데이터
+command_list = [
+    "add : 데이터 추가",
+    "list : 데이터 조회",
+    "update : 데이터 수정",
+    "delete : 데이터삭제",
+]
 
-# print("s의 길이 출력")
-# print(len(s))
+# 데이터 저장 리스트
+list1 = []
 
-# print("s를 이루는 문자 중에서 앞에서 문자 2개 출력")
-# print(s[0:2])
 
-# print("s를 이루는 문자 중에서 앞에서 문자 하나 건너 뛰고 1개 출력")
-# print(s[1:2])
+# 함수
+# help
+def print_help():
+    for comm in command_list:
+        print(comm)
 
-# print("s를 이루는 문자 중에서 앞에서 문자 4개 출력, 첫 인덱스 생략")
-# print(s[:4])
 
-# print("s를 이루는 문자 중에서 앞에서 문자 3개 건너뛰고, 나머지 전부 출력, len")
-# print(s[3:len(s)])
+# add
+def add_data():
+    print("저장할 값을 입력해주세요 : ")
+    a = input()
+    list1.append(a)
 
-# print("s를 이루는 문자 중에서 앞에서 문자 3개 건너뛰고, 나머지 전부 출력, 뒤 인덱스 생략, 방법 2")
-# print(s[3:])
 
-# print("== 예제 2 ==")
-# s = "안녕하세요"
+# update
+def update_data():
+    print("몇번째 값을 수정하시겠습니까 : ")
+    a = int(input())
+    print("어떤 값으로 수정하시겠습니까? : ")
+    b = input()
+    list1[a - 1] = b
 
-# print("s를 이루는 문자를 하나씩 출력, while")
-# i = 0
-# while i < len(s):
-#     print(s[i])
-#     i += 1
 
-# print("s를 이루는 문자를 역순으로 하나씩 출력, while")
-# i = len(s) -1
-# while i >= 0:
-#     print(s[i])
-#     i -= 1
+# delete
+def delete_data():
+    print("몇 번째 값을 삭제하시겠습니까? :")
+    a = int(input())
 
-# print("s를 이루는 문자를 하나씩 출력, for")
-# for c in s:
-#     print(c)
+    if a > len(list1):
+        print("없는 데이터 입니다.")
+    else:
+        del list1[a - 1]
+        print(f"{a}번째 값을 삭제 했습니다.")
 
-# print("== 예제 3 ==")
-# s = "a안b녕c하d세e요f"
 
-# print("s를 이루는 문자 중에서 한글만 출력")
-# print(s[1] + s[3] + s[5] + s[7] + s[9])
+# 메인 프로세스
+while True:
+    print("명령어를 입력해주세요 : ")
+    comm = input()
 
-# print("s를 이루는 문자 중에서 한글만 출력, for, if or, continue")
-# for c in s:
-#     if c == "a" or c == "b" or c == "c" or c == "d" or c == "e" or c == "f":
-#         # 현재 문자가 a ~ f중에 하나인지 확인
-#         continue  # 위 조건이 참이면 현재 루프를 건너 뛴다.
-#     print(c)
-
-# print("s를 이루는 문자 중에서 한글만 출력, for, if and, 방법 2")
-# for c in s:
-#     if c != "a" and c != "b" and c != "c" and c != "d" and c != "e" and c != "f":
-#         print(c)
-
-# print("s를 이루는 문자 중에서 한글만 출력, for, not in, 방법 3")
-# for c in s:
-#     if c not in ["a", "b", "c", "d", "e", "f"]: # 현재 문자가 a ~ f 리스트에 있는지 없는지 확인
-#         print(c)
-
-# continue
-# print("== continue ==")
-# for i in range(1, 6):
-#     if i == 3:
-#         continue
-#     print(i)
-
-# break
-# print("== break ==")
-# for i in range(1, 6):
-#     if i == 3:
-#         break
-#     print(i)
+    if comm == "exit":
+        print("프로그램이 종료 되었습니다.")
+        break
+    elif comm == "help":
+        print_help()
+    elif comm == "add":
+        add_data()
+    elif comm == "list":
+        print(list1)
+    elif comm == "update":
+        update_data()
+    elif comm == "delete":
+        delete_data()
