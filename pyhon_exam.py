d91f3e5d84b67d602a878a6c4bb9848b0558f2a2
gykim93 <gykim1993@gmail.com>
2026-01-13 10:46:44 +0900

    회원 정보를 저장하는 딕셔너리

--- pyhon_exam.py
+++ pyhon_exam.py
@@ -1,53 +1,68 @@
-# 데이터 여러개를 저장할 때
-# 리스트
-# 딕셔너리
+# 딕셔너리를 이용해서 회원 정보를 저장해주세요. 회원 정보는 아이디, 비밀번호, 이름으로 하겠습니다.
+# 아이디가 hong123, 비밀번호가 1234, 이름이 홍길동인 회원
+# 아이디가 sony7, 비밀번호가 7777, 이름이 손흥민인 회원
+# 아이디가 ryu99, 비밀번호가 9999, 이름이 류현진인 회원
+# 위 세명의 회원을 딕셔너리를 이용해 만들고 출력해주세요. 회원의 정보를 모두 출력해주세요.
 
-# 리스트의 장점?
-# 데이터를 넣을 때 편하다.
-# 데이터를 넣으면 키가 자동으로 정해진다.
+"""
+아이디 : hong123
+비밀번호 : 1234
+이름 : 홍길동
+================
+아이디 : sony7
+비밀번호 : 7777
+이름 : 손흥민
+================
+아이디 : ryu99
+비밀번호 : 9999
+이름 : 류현진
+================
+"""
+user1 = {"아이디": "hong123", "비밀번호": "1234", "이름": "홍길동"}
+user2 = {"아이디": "sony7", "비밀번호": "7777", "이름": "손흥민"}
+user3 = {"아이디": "ryu99", "비밀번호": "9999", "이름": "류현진"}
 
-# 영희, 영수, 철수
-# ages = [10, 20, 30]
-# print(ages[0])  # 영희
-# print(ages[1])  # 영수
-# print(ages[2])  # 철수
+user_list = [user1, user2, user3]
+# 모든 회원 정보 출력
+def print_all_users():
+    for user in user_list:
+        for key,value in user.items():
+            print(f"{key} : {value}")
+        print("================")
+# print_all_users()
+# # 모든 회원 아이디를 출력
+# for user in user_list:
+#     print(user["아이디"])
 
-ages1 = {"영희": 10, "영수": 20, "철수": 30}
-# print(ages1["영희"])
-# # 민희 나이추가
-# ages1["민희"] = 40
+# hong123 아이디를 가진 회원의 이름 출력
+# for user in user_list:
+#     if user["아이디"] == "hong123":
+#         print(user["이름"])
 
-# print(ages1)
+# hong123 아이디를 가진 회원의 비밀번호를 3333으로 수정 후 모든 회원 정보 출력
+# for user in user_list:
+#     if user["아이디"] == "hong123":
+#         user["비밀번호"] = "3333"
+# print_all_users()
 
-# 딕셔너리 장점
-# 데이터를 가져올 때 편하다.
-# 단점 => 딕셔너리에 데이터를 넣을 때는 키값을 내가 직접 넣어줘야된다.
-# ages1["영수"] = 50 => 위 딕셔너리에 키 값이 존재하면 추가가 아니고, 수정
+# 아이디가 hong124, 이름 홍길순, 비밀번호 h1234 인 회원추가
+user5 = {"아이디": "hong124", "비밀번호": "h1234", "이름": "홍길순"}
+user_list.append(user5)
+# print_all_users()
 
-# 반복출력
-print("== 딕셔너리 반복1 ==")  # 키 값만 나온다.
-for name in ages1:  # 기본적으로 딕셔너리를 순회하면 key값만 얻을 수 있다.
-    print(name)
+# 아이디가 중복될 시 추가 거부.
+user5 = {}
+user5["아이디"] = "hong123"
+user5["비밀번호"] = "h1234"
+user5["이름"] = "홍길"
 
-print("== 딕셔너리 반복2 ==")
-# for문을 사용해서 딕셔너리에 key를 순회한다. name 딕셔너리의 key => 영희, 영수, 철수
-# for문 안쪽 age 변수 => ages1 딕셔너리에 해당하는 이름에 벨류가 할당이 된다.
-for name in ages1:
-    age = ages1[name]
-    print(f"{name} : {age}")
-
-print("== 딕셔너리 반복3 ==")  # 누구의 나이인지 모른다.
-for age in ages1.values():  # values() 해당 딕셔너리의 벨류값만 얻고 for을 순회한다.
-    print(f"{age}")
-
-print("== 딕셔너리 반복4 ==")  # 가장 깔끔한 방법
-for name, age in ages1.items():  # items() 사용하면 키와 벨류값을 동시에 얻는다.
-    # name => 키값, age => 벨류값
-    print(f"이름은 {name} 이고, 나이는 {age}살")
-
-# 삭제
-del ages1["철수"]
-
-print("== 딕셔너리 반복4 ==")
-for name, age in ages1.items():
-    print(f"이름은 {name} 이고, 나이는 {age}살")
+flag = True
+for user in user_list:
+    if user["아이디"] == user5["아이디"]:
+        flag = False
+        print("이미 존재하는 아이디 입니다.")
+        break
+if flag:
+    user_list.append(user5)    
+    
+print_all_users()    
