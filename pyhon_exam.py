b0b3ff878f05729a9dbdf1e2e54a8a349adeb404
gykim93 <gykim1993@gmail.com>
2026-01-15 10:18:32 +0900

    전사클래스 만들기

--- pyhon_exam.py
+++ pyhon_exam.py
@@ -1,12 +1,31 @@
-class Person: # 비공개 속성 => 클래스 외부에서 접근이 불가, 클래스 내부에서만 사용가능 => private attribute
-    def __init__(self, name, age, weight):
-        self.hello = "안녕하세요"
-        self.name = name
-        self.age = age
-        # self.weight = weight
-        self.__weight = weight # 변수 앞에 `__`를 붙혀서 비공개 속성으로 변경
+# class Warrior():
+#   def __init__(self, hp, mp, attack_point):
+#       self.hp = hp
+#       self.mp = mp
+#       self.attack_point = attack_point
 
-Jin = Person("진", 22, 72)        
-print(Jin.name)
-print(Jin.age)
-print(Jin.weight)
+#   def attack(self):
+#       print("공격!")
+
+# x = Warrior(1000,500,30)
+
+# print(x.hp,x.mp,x.attack_point)
+
+# x.attack()
+# # 출력 : 공격!
+
+
+class Warrior:
+    def attack(self):
+        print("공격!")
+
+
+x = Warrior()
+x.hp = 1000
+x.mp = 500
+x.attack_point = 30
+
+print(x.hp, x.mp, x.attack_point)
+
+x.attack()
+# 출력 : 공격!
