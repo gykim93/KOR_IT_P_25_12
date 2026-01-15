48390b7b5592601a3d97fd2e4f5a4ec07c8602e2
gykim93 <gykim1993@gmail.com>
2026-01-15 10:28:10 +0900

    인스턴스, 스태틱(정적), 클래스 메서드

--- pyhon_exam.py
+++ pyhon_exam.py
@@ -1,31 +1,38 @@
-# class Warrior():
-#   def __init__(self, hp, mp, attack_point):
-#       self.hp = hp
-#       self.mp = mp
-#       self.attack_point = attack_point
+print("== 인스턴스 메서드 ==")
+# 인스턴스 메서드
+class Person():
+    def hi(self):
+        print("hi")
 
-#   def attack(self):
-#       print("공격!")
+Jin = Person()
+Jin.hi()        
 
-# x = Warrior(1000,500,30)
+print("== 스태틱(정적) 메서드 ==")
 
-# print(x.hp,x.mp,x.attack_point)
+# 정적 메서드 => 첫 번째 매개변수로 self는 필요없다.
+class Calc():
+    @staticmethod # 스태틱(정적) 메서드
+    def add(a, b):
+        print(a + b)
 
-# x.attack()
-# # 출력 : 공격!
+Calc.add(10, 20) # 클래스에서 바로 메서드(함수) 호출(실행)이 가능하다.
 
+print("== 클래스 메서드 ==")
 
-class Warrior:
-    def attack(self):
-        print("공격!")
+# 클래스 메서드
+class Person2():
+    count = 0 # 클래스 속성
+    def __init__(self):
+        Person2.count += 1
+    
+    @classmethod
+    def print_count(cls): #클래스메서드
+        print(f"사람이 총 {cls.count}명 있습니다.") # cls로 클래스 속성에 접근한다.
 
+Person2.print_count()        
+Jin2 = Person2()
 
-x = Warrior()
-x.hp = 1000
-x.mp = 500
-x.attack_point = 30
+Person2.print_count()  
+Jin2 = Person2()      
 
-print(x.hp, x.mp, x.attack_point)
-
-x.attack()
-# 출력 : 공격!
+Person2.print_count()  
